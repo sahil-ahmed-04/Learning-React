b8b1fa71cc66bcaeb10fa3170e0910b684cbf51e
Hesd-Hunter <hesdhunter7@gmail.com>
2025-08-10 20:05:46 +0530

    another commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,28 @@
 import './App.css';
 
 function App() {
+  let name="react app"
+  let className="App-header"
+  console.log(App)
+  return (
+    <div className="">
+      <div className={className}>
+        {name}
+        <h1>Hello World</h1>
+        <Demo></Demo>
+      </div>
+      
+    </div>
+  );
+}
+
+function Demo() {
+  console.log(Demo)
   return (
     <div className="App">
       <div className="App-header">
-        <h1>Hello World</h1>
-      </div>
-      
+        <h1>Demo</h1>
+      </div> 
     </div>
   );
 }
